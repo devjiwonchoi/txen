f43d25ae61d8eac8ede89dd3ac01f62bd0c4ce06
devjiwonchoi <devjiwonchoi@gmail.com>
2024-05-02 04:02:55 +0900

    hotfix: callback signin email to match my email

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,4 +3,9 @@ import GitHub from 'next-auth/providers/github'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  callbacks: {
+    signIn: async ({ user }) => {
+      return user.email === 'devjiwonchoi@gmail.com'
+    },
+  },
 })
