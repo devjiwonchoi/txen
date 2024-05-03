4f23ab5223f9b39f7de41e5041445a68b14ccdd2
devjiwonchoi <devjiwonchoi@gmail.com>
2024-05-04 02:00:16 +0900

    wip

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+import arg from 'arg'
+
+const args = arg({
+  // Types
+  '--help': Boolean,
+  '--version': Boolean,
+  '--verbose': arg.COUNT, // Counts the number of times --verbose is passed
+  '--port': Number, // --port <number> or --port=<number>
+  '--name': String, // --name <string> or --name=<string>
+  '--tag': [String], // --tag <string> or --tag=<string>
+
+  // Aliases
+  '-v': '--verbose',
+  '-n': '--name', // -n <string>; result is stored in --name
+  '--label': '--name', // --label <string> or --label=<string>;
+  //     result is stored in --name
+})
