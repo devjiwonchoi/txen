3fdaf999d41ec1d72a22241f2cf66b4de9d5fa6f
devjiwonchoi <devjiwonchoi@gmail.com>
2024-05-23 00:58:13 +0900

    fix: api

--- api/sections.ts
+++ api/sections.ts
@@ -5,17 +5,25 @@ import { getSectionsQuery } from './_get-sections-query'
 const edgedb = createHttpClient()
 
 export async function POST(request: Request) {
-  const { embedding } = (await request.json()) as { embedding: Embedding }
-  const sections = await getSectionsQuery.run(edgedb, {
-    target: embedding,
-    matchThreshold: 0.3,
-    matchCount: 8,
-    minContentLength: 20,
-  })
+  try {
+    const { embedding } = (await request.json()) as { embedding: Embedding }
+    const sections = await getSectionsQuery.run(edgedb, {
+      target: embedding,
+      matchThreshold: 0.3,
+      matchCount: 8,
+      minContentLength: 20,
+    })
 
-  return new Response(JSON.stringify({ sections }), {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
+    return new Response(JSON.stringify({ sections }), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      status: 200,
+    })
+  } catch (error) {
+    console.error(error)
+    return new Response('Something went wrong', {
+      status: 403,
+    })
+  }
 }
--- src/ai/get-sections.ts
+++ src/ai/get-sections.ts
@@ -1,20 +1,26 @@
 import type { Embedding } from 'ai'
 import type { Section } from '../types'
+import { API_URL } from '../constants'
 import 'dotenv/config'
 
 export async function getSections(embedding: Embedding) {
   try {
-    const response = await fetch(`${process.env.API_URL}/sections`, {
+    const response = await fetch(`https://txenext.vercel.app/api/sections`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ embedding }),
     })
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch sections: ${response.statusText}`)
+    }
+
     const data = (await response.json()) as { sections: Section[] }
 
     return data.sections
   } catch (error) {
-    throw new Error('Failed to fetch sections:', error as Error)
+    console.error(error)
   }
 }
