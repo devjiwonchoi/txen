eb52cce8efdf811ed59b0e9741ee3faf1ac24ecb
Jiwon Choi <devjiwonchoi@gmail.com>
2024-05-24 04:02:55 +0900

    dump to sections (#22)

--- api/sections.ts
+++ api/sections.ts
@@ -1,14 +1,44 @@
 import type { Embedding } from 'ai'
 import { createHttpClient } from 'edgedb'
-import { getSectionsQuery } from './_get-sections-query'
+import edgeql from '../dbschema/edgeql-js'
 
-const edgedb = createHttpClient()
+export const getSectionsQuery = edgeql.params(
+  {
+    target: edgeql.OpenAIEmbedding,
+    matchThreshold: edgeql.float64,
+    matchCount: edgeql.int16,
+    minContentLength: edgeql.int16,
+  },
+  (params) => {
+    return edgeql.select(edgeql.Section, (section) => {
+      const dist = edgeql.ext.pgvector.cosine_distance(
+        section.embedding,
+        params.target
+      )
+      return {
+        content: true,
+        tokens: true,
+        dist,
+        filter: edgeql.op(
+          edgeql.op(edgeql.len(section.content), '>', params.minContentLength),
+          'and',
+          edgeql.op(dist, '<', params.matchThreshold)
+        ),
+        order_by: {
+          expression: dist,
+          empty: edgeql.EMPTY_LAST,
+        },
+        limit: params.matchCount,
+      }
+    })
+  }
+)
 
 export async function POST(request: Request) {
   try {
     const { embedding } = (await request.json()) as { embedding: Embedding }
 
-    const sections = await getSectionsQuery.run(edgedb, {
+    const sections = await getSectionsQuery.run(createHttpClient(), {
       target: embedding,
       matchThreshold: 0.3,
       matchCount: 8,
