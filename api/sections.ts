0ad43318337103e911603c9a59e010c179ce3b3b
Jiwon Choi <devjiwonchoi@gmail.com>
2024-05-24 04:48:30 +0900

    dist bundle (#24)

--- api/sections.ts
+++ api/sections.ts
@@ -1,38 +1,6 @@
 import type { Embedding } from 'ai'
 import { createHttpClient } from 'edgedb'
-import edgeql from '../public/edgeql-js'
-
-export const getSectionsQuery = edgeql.params(
-  {
-    target: edgeql.OpenAIEmbedding,
-    matchThreshold: edgeql.float64,
-    matchCount: edgeql.int16,
-    minContentLength: edgeql.int16,
-  },
-  (params) => {
-    return edgeql.select(edgeql.Section, (section) => {
-      const dist = edgeql.ext.pgvector.cosine_distance(
-        section.embedding,
-        params.target
-      )
-      return {
-        content: true,
-        tokens: true,
-        dist,
-        filter: edgeql.op(
-          edgeql.op(edgeql.len(section.content), '>', params.minContentLength),
-          'and',
-          edgeql.op(dist, '<', params.matchThreshold)
-        ),
-        order_by: {
-          expression: dist,
-          empty: edgeql.EMPTY_LAST,
-        },
-        limit: params.matchCount,
-      }
-    })
-  }
-)
+import { getSectionsQuery } from '../dist/get-sections-query'
 
 export async function POST(request: Request) {
   try {
