d9b8cb761ac7a176e179eb928d771d80a2971497
devjiwonchoi <devjiwonchoi@gmail.com>
2024-05-25 20:34:21 +0900

    chore: merge get sections to api

--- api/sections.ts
+++ api/sections.ts
@@ -1,6 +1,38 @@
 import type { Embedding } from 'ai'
 import { createHttpClient } from 'edgedb'
-import { getSectionsQuery } from './_get-sections-query'
+import edgeql from '../dbschema/edgeql-js'
+
+export const getSectionsQuery = edgeql.params(
+  {
+    target: edgeql.OpenAIEmbedding,
+    matchThreshold: edgeql.float64,
+    matchCount: edgeql.int16,
+    minContentLength: edgeql.int16,
+  },
+  (params) => {
+    return edgeql.select(edgeql.Section, (section) => {
+      const dist = edgeql.ext.pgvector.cosine_distance(
+        section.embedding,
+        params.target
+      )
+      return {
+        content: true,
+        tokens: true,
+        dist,
+        filter: edgeql.op(
+          edgeql.op(edgeql.len(section.content), '>', params.minContentLength),
+          'and',
+          edgeql.op(dist, '<', params.matchThreshold)
+        ),
+        order_by: {
+          expression: dist,
+          empty: edgeql.EMPTY_LAST,
+        },
+        limit: params.matchCount,
+      }
+    })
+  }
+)
 
 export async function POST(request: Request) {
   try {
